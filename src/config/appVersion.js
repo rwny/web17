12516b50b024e1cfab5b59e81e9cd61e5e180ea2
rwny <rawee6426@gmail.com>
2025-03-14 01:09:23 +0700

    1018 0109

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -2,4 +2,4 @@
  * Application version information
  * Update this file to change version display across the entire application
  */
-export const APP_VERSION = 'rw . 1015';
+export const APP_VERSION = 'rw . 1015 0109';
